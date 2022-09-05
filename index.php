f85988420216ef0071bf6ce8ac9062ef8176f25d
BaptisteQnt <baptiste.qnt@outlook.com>
2022-09-05 11:30:28 +0200

    hello world

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <title>Ma page <?php echo 'Projet POO Blog'; ?></title>
 </head>
 <body>
+<h2>Test push voila</h2>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <!-- <a class="navbar-brand" href="#"></a> -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
